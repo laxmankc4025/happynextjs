4dac5b5612680622b2b986c9c7e32e6d4418809d
laxmankc4025 <70631354+laxmankc4025@users.noreply.github.com>
2022-01-01 19:35:18 +0545

    update

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div>
-      <footer class="bg-gray-200 text-center lg:text-left 6 ">
+      <footer className="bg-gray-200 text-center lg:text-left 6 ">
         <div
-          class="text-gray-700 text-center p-4"
+          className="text-gray-700 text-center p-4"
           //   style="background-color: rgba(0, 0, 0, 0.2);"
         >
           © 2022 Copyright:
           <Link href="http://localhost:3000/">
-            <a class="text-gray-800" href="http://localhost:3000/">
+            <a className="text-gray-800" href="http://localhost:3000/">
               Localhost
             </a>
           </Link>
